802fc6814cf755737394c1e85cc03a1bb5831fd2
xd88dx <xd88dx@foxmail.com>
2025-09-02 23:55:42 +0800

    1.9.9

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 set -e
 
-if [ ! -f "./src/worker.js" ]; then
-  echo "未检测到 src/worker.js，执行初始化..."
+if [ ! -f "./src/index.js" ]; then
+  echo "未检测到 src/index.js，执行初始化..."
   npm run init
 else
-  echo "检测到 src/worker.js，启动 wrangler..."
+  echo "检测到 src/index.js，启动 wrangler..."
   npm run start
 fi
